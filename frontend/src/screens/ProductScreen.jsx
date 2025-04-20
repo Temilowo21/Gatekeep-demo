5f68cc3edab395337d230f0086dd12a1cd884156
Temilowo21 <k2273315@kingston.ac.uk>
2025-04-20 15:40:24 +0100

    redux done

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 import "../components/ProductScreen.css";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const ProductScreen = () => {
     const { id: productId } = useParams();
@@ -16,9 +18,9 @@ const ProductScreen = () => {
             </Link>
 
             { isLoading ? (
-                <h2>Loading...</h2>
+                <Loader />
             ) : error ? (
-                <div>{error?.data?.message || error.error}</div>
+                <Message variant='danger'>{ error?.data?.message || error.error}</Message>
             ) : (
                 <Row>
                 <Col md={5}>
